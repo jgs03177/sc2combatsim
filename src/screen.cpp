1da964471e01a486793e94decc960357d4a223fc
Donghyeon Lee <jgs03177@gmail.com>
2019-08-22 18:00:35 +0900

    Code quality..

--- src/screen.cpp
+++ src/screen.cpp
@@ -1,16 +1,8 @@
 #include "lodepng.h"
-#include <iostream>
 #include "sim_bot.h"
 #include "sc2renderer/sc2_renderer.h"
+#include <iostream>
 #include <vector>
-
-void encodeOneStep(const char* filename, std::vector<unsigned char>& image, unsigned width, unsigned height) {
-	//Encode the image
-	unsigned error = lodepng::encode(filename, image, width, height);
-
-	//if there's an error, display it
-	if (error) std::cout << "encoder error " << error << ": " << lodepng_error_text(error) << std::endl;
-}
 
 // must be in render mode
 void SimBot::screencapture(const std::string& filename, int compression) {
@@ -28,18 +20,14 @@ void SimBot::screencapture(const std::string& filename, int compression) {
 	int32_t height = mapraw.size().y();
 	int32_t length = map.length();
 
-	//std::cout << width << height << length << std::endl;
-	std::vector<unsigned char> map_RGB(length, 255);
+	std::vector<unsigned char> map_RGB(length);
 	for (int i = 0; i < length; i += 3) {
 		//BRG to RGB
 		map_RGB[i    ] = map[i + 2];
 		map_RGB[i + 1] = map[i + 1];
 		map_RGB[i + 2] = map[i    ];
 	}
-	std::vector<unsigned char> a;
-	unsigned error;
-	//error = lodepng::encode(filename, (unsigned char*)mapraw.data().data(), width, height, LCT_RGB, 8);
-	//if (error) std::cout << "decoder1 error " << error << ": " << lodepng_error_text(error) << std::endl;
-	error = lodepng::encode(filename, (unsigned char *)map_RGB.data(), width, height, LCT_RGB, 8);
-	if (error) std::cout << "decoder2 error " << error << ": " << lodepng_error_text(error) << std::endl;
+
+	unsigned error = lodepng::encode(filename, (unsigned char *)map_RGB.data(), width, height, LCT_RGB, 8);
+	if (error) std::cout << "decoder error " << error << ": " << lodepng_error_text(error) << std::endl;
 }
--- src/sim_bot.h
+++ src/sim_bot.h
@@ -14,6 +14,8 @@ class SimBot : public Agent {
 private:
 	std::string botname;
 	Vector2D offset;
+	Vector2D offset1;
+	Vector2D offset2;
 	Point2D genpos;
 	Point2D centerpos;
 	Combinator combinator;
@@ -22,71 +24,29 @@ public:
 	SimBot(std::string name, Vector2D offset) :
 		Agent(), botname(name), offset(offset) {};
 
+	SimBot(std::string name, Vector2D offset1, Vector2D offset2) :
+		Agent(), botname(name), offset1(offset1), offset2(offset2) {};
+
 	~SimBot() {};
 
-	virtual void OnGameStart() final override {
-		std::cout << botname << " Hello, World!" << std::endl;
-		const GameInfo game_info = Observation()->GetGameInfo();
-		centerpos = (game_info.playable_max + game_info.playable_min) / 2.0f;
-		//centerpos = Point2D(game_info.width / 2.0f, game_info.height / 2.0f);
-		genpos = offset + centerpos;
-		combinator.SetUnitData(Observation());
-	}
+	void OnGameStart() final override;
 
-	virtual void OnUnitIdle(const Unit* unit) final override {
-		const ObservationInterface* observation = Observation();
-		ActionInterface* actions = Actions();
-		//findnearestenemy
-		Units allyunits = observation->GetUnits(Unit::Alliance::Self);
-		Units enemyunits = observation->GetUnits(Unit::Alliance::Enemy);
-		Point2D enemycenter;
-		if (enemyunits.size()) {
-			for (const auto& unit : enemyunits) {
-				enemycenter += Point2D(unit->pos);
-			}
-			enemycenter /= static_cast<float>(enemyunits.size());
-		}
-		else {
-			enemycenter = centerpos;
-		}
-		actions->UnitCommand(unit, ABILITY_ID::ATTACK, enemycenter);
-	}
+	void OnUnitIdle(const Unit* unit) final override;
 
-	virtual void OnStep() final override {
-		const ObservationInterface* observation = Observation();
-		//Units liberators = observation->GetUnits(Unit::Alliance::Self, IsUnit(UNIT_TYPEID::TERRAN_LIBERATOR));
-		//Actions()->UnitCommand(liberators, ABILITY_ID::MORPH_LIBERATORAGMODE, Point2D(15.0f, 22.5f));
-		//size_t medivac_size = observation->GetUnits(Unit::Alliance::Self, IsUnit(UNIT_TYPEID::TERRAN_MEDIVAC)).size();
-		//size_t my_units_size = observation->GetUnits(Unit::Alliance::Self).size();
-		//size_t enemy_units_size = observation->GetUnits(Unit::Alliance::Enemy).size();
-		//if (enemy_units_size && my_units_size == medivac_size) {
-		//	d_killunit();
-		//}
-		Units allyunits = observation->GetUnits(Unit::Alliance::Self);
-		Point2D allycenter;
-		if (allyunits.size()) {
-			for (const auto& unit : allyunits) {
-				allycenter += Point2D(unit->pos);
-			}
-			allycenter /= static_cast<float>(allyunits.size());
-		}
-		else {
-			allycenter = genpos;
-		}
-		Debug()->DebugMoveCamera(allycenter);
-		Debug()->SendDebug();
-	}
+	void OnStep() final override;
 
-	size_t CountUnitType(UnitTypeID unit_type) {
-		return Observation()->GetUnits(Unit::Alliance::Self, IsUnit(unit_type)).size();
-	}
-	size_t CountUnitType() {
-		return Observation()->GetUnits(Unit::Alliance::Self).size();
-	}
+	size_t CountUnitType(UnitTypeID unit_type, uint32_t playerID);
+	size_t CountUnitType(UnitTypeID unit_type);
+	size_t CountUnitType(uint32_t playerID);
+	size_t CountUnitType();
 
+	void d_countunits(Json::Value& squad, uint32_t playerID);
 	void d_countunits(Json::Value& squad);
+	bool d_deployunit(UnitTypeID unit, uint32_t numbers, Vector2D pos, uint32_t playerID);
 	bool d_deployunit(UnitTypeID unit, uint32_t numbers);
+	bool d_deployunit(Json::Value squad, Vector2D pos, uint32_t playerID);
 	bool d_deployunit(Json::Value squad);
+	void d_killunit(uint32_t playerID);
 	void d_killunit();
 	void d_init();
 	void d_finish();
